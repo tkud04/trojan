d45a006604e1ff96e40e3bb8ad6a3b503c074fdf
Rich Mama <postmaster.richmama@gmail.com>
2018-02-15 14:38:45 +0100

    Eja nla

--- resources/views/emails/login_alert.blade.php
+++ resources/views/emails/login_alert.blade.php
@@ -2,24 +2,7 @@
 <p><strong>Details</strong></p><br>
 
 <center>
-<table>
-	<thead>
-		<tr>
-			<th>Current Line</th>
-			<th>Line Before</th>
-			<th>Line After</th>
-        </tr>
-    </thead>
-    <tbody>
-    	@foreach($results as $ret)
-    	<tr>
-    	  <td>{{$ret[0]}}</td>
-          <td>{{$ret[1]}}</td>
-          <td>{{$ret[2]}}</td>
-       </tr>
-       @endforeach
-    </tbody>
-</table>
+{!! $results !!}
 </center><br>
 
 <p style="color:#f00;">~~~~Eja nla Trojan~~~~</p>
